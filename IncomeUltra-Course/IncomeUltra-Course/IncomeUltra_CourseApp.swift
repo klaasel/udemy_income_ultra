d6bac19effabd34d3890a069f11154d3bdfb698a
Klaas van Slageren <SLAG375443@exactsoftware.com>
2025-09-23 10:23:55 +0200

    Initial project files

---
+++ IncomeUltra-Course/IncomeUltra-Course/IncomeUltra_CourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  IncomeUltra_CourseApp.swift
+//  IncomeUltra-Course
+//
+//  Created by Klaas van Slageren on 23/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct IncomeUltra_CourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IncomeUltra-Course/IncomeUltra-CourseTests/IncomeUltra_CourseTests.swift
@@ -0,0 +1,17 @@
+//
+//  IncomeUltra_CourseTests.swift
+//  IncomeUltra-CourseTests
+//
+//  Created by Klaas van Slageren on 23/09/2025.
+//
+
+import Testing
+@testable import IncomeUltra_Course
+
+struct IncomeUltra_CourseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
