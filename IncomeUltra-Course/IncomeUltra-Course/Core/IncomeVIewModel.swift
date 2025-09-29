9ad0755f3cce3006155d3d9564483d51b34d41cc
Klaas van Slageren <SLAG375443@exactsoftware.com>
2025-09-29 10:42:45 +0200

    IncomeView

---
+++ IncomeUltra-Course/IncomeUltra-Course/Core/IncomeVIewModel.swift
@@ -0,0 +1,20 @@
+//
+//  IncomeVIewModel.swift
+//  IncomeUltra-Course
+//
+//  Created by Klaas van Slageren on 26/09/2025.
+//
+
+import Foundation
+import Combine
+import Factory
+
+@MainActor
+final class IncomeViewModel: ObservableObject {
+    @Injected(\.incomeStore) var incomeStore
+    @Injected(\.appInfoStore) var appInfoStore
+    
+    var appName: String {
+        appInfoStore.name
+    }
+}
--- IncomeUltra-Course/IncomeUltra-Course/Core/IncomeView.swift
+++ IncomeUltra-Course/IncomeUltra-Course/Core/IncomeView.swift
@@ -8,11 +8,41 @@
 import SwiftUI
 
 struct IncomeView: View {
+    @StateObject private var viewModel = IncomeViewModel()
+    
     var body: some View {
-        Text("Income View")
+        incomeView
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar{
+                toolbarItems
+            }
+    }
+}
+
+private extension IncomeView {
+    var incomeView: some View {
+        ScrollView {
+            VStack(spacing: 16){
+                
+            }
+        }
+    }
+    
+    @ToolbarContentBuilder
+    var toolbarItems: some ToolbarContent {
+        ToolbarItem(placement: .principal) {
+            HStack(spacing: 5) {
+                Image(systemName: "dollarsign.circle")
+                    .foregroundStyle(Color.appTheme.accent)
+                Text(viewModel.appName)
+            }
+            .fontWeight(.semibold)
+        }
     }
 }
 
 #Preview {
-    IncomeView()
+    NavigationStack {
+        IncomeView()
+    }
 }
